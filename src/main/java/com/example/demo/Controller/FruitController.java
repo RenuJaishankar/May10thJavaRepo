a7578fb2e5bf95166546adf51dba73e38a62361f
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-05-10 18:57:05 -0400

    new files added

---
+++ src/main/java/com/example/demo/Controller/FruitController.java
@@ -0,0 +1,21 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Service.FruitService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class FruitController {
+   @Autowired
+    FruitService fruitService;
+    @RequestMapping(value="/fruits", method= RequestMethod.GET)
+    ModelAndView getFruits(){
+    return fruitService.getFruits();
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/Repo/FruitRepo.java
@@ -0,0 +1,7 @@
+package com.example.demo.Repo;
+
+import com.example.demo.Model.Fruit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FruitRepo extends CrudRepository<Fruit,Long> {
+}
